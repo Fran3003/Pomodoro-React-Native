66908e0ce2dae4a20ecffb13d6fd51923dedfe17
Fran3003 <franco.rantucho@gmail.com>
2023-10-18 10:21:10 -0300

    Header y Timer

--- App.js
+++ App.js
@@ -1,20 +1,74 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, Platform, TouchableOpacity } from 'react-native';
+import { useEffect, useState } from 'react';
+import Header from './src/components/Header';
+import Timer from './src/components/Timer';
+
+
+const colors = ["#E36B2C","#23BAC4","#8C4966"]
 
 export default function App() {
+  const [isWorking, setIsWorking] = useState(false);
+  const [time, setTime] = useState(25 * 60);
+  const [currentTime, setCurrentTime] = useState("POMO" | "SHORT" | "BREAK");
+  const [isActive, setIsActive] = useState(false);
+
+  useEffect(() => {
+    let interval = null;
+
+    if (isActive) {
+      interval = setInterval(() => {
+        setTime(time - 1)
+      }, 1000);
+    }
+    else {
+      clearInterval(interval);
+    }
+
+    if (time === 0) {
+      setIsActive(false);
+      setIsWorking((prev) => !prev);
+      setTime(isWorking ? 300 : 1500)
+    }
+ 
+    return () => clearInterval(interval);
+  }, [isActive, time])
+
+  const handleStartStop = () => {
+    setIsActive(!isActive);
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={[styles.container, {backgroundColor: colors[currentTime]}]}>
+      <View style={{
+          flex: 1, 
+          paddingHorizontal: 15, 
+          paddingTop: Platform.OS === "android" && 30,
+        }}>
+        <Text style={styles.text}> Pomodoro </Text>
+        <Header currentTime={currentTime} setCurrentTime={setCurrentTime} setTime={setTime}/>
+        <Timer time={time}/>
+        <TouchableOpacity onPress={handleStartStop} style={styles.button}>
+          <Text style={{color: "white", fontWeight: 'bold'}}>{isActive ? "STOP" : "START"}</Text>
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  text: {
+    fontSize: 32,
+    fontWeight: "bold",
+    textAlign:'center'
+  },
+  button: {
+    backgroundColor: "#474b4e",
+    padding: 15,
+    marginTop: 15,
+    borderRadius: 15,
+    alignItems: 'center',
+  }
 });
